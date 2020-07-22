f183656cc48ff559b1f9d5ffb6fcbf8e6b3df322
OlegBirykov <p_petro75@mail.ru>
2020-07-22 21:48:17 +0300

    Добавлен код и тесты

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,5 +1,16 @@
 import phoneNormalize from '../app';
 
-test('', () => {
-  expect(phoneNormalize('')).toEqual('');
+test('Номер должен содержать только цифры и символ "+"', () => {
+  expect(phoneNormalize('+7 927 000 00 00')).toBe('+79270000000');
+  expect(phoneNormalize('+86 000 000 0000')).toBe('+860000000000');
+});
+
+test('Номер должен начинаться с кода страны', () => {
+  expect(phoneNormalize('5 (927) 000-00-00')).toBe('+59270000000');
+  expect(phoneNormalize('86 000 000 0000')).toBe('+860000000000');
+});
+
+test('Десятизначный номер без кода страны или после восьмёрки должен быть российским', () => {
+  expect(phoneNormalize(' (927) 000-00-00')).toBe('+79270000000');
+  expect(phoneNormalize('8 (927) 000-00-00')).toBe('+79270000000');
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,6 @@
 export default function phoneNormalize(phone) {
-  return phone;
+  return phone.replace(/[^\d+]/g, '')
+    .replace(/^(\d{10})$/, '+7$1')
+    .replace(/^8(\d{10})$/, '+7$1')
+    .replace(/^(\d+)$/, '+$1');
 }
